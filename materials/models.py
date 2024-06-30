afb45d1ed5f87063888ed4927533db82931611ed
Svyatoslav Deinekin <svyadey@mail.ru>
2024-06-30 03:30:14 +0300

    drf hw1

---
+++ materials/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название курса")
+    description = models.TextField(verbose_name="Описание курса")
+    preview_image = models.ImageField(
+        upload_to="course_images", verbose_name="Фото", **NULLABLE
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.description[:100]}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание урока")
+    preview_image = models.ImageField(
+        upload_to="course_images", verbose_name="Фото", **NULLABLE
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Курс", **NULLABLE
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.description[:100]}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
